d8e5f3f0a8165c286a420f929f82082c8606fb54
Ewir Rodriguez <ewirjose1@gmail.com>
2019-07-14 14:21:00 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+var bodyParser = require('body-parser')
 
 
 // Inicializar variables
 var app = express();
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }, (err, res) => {
     if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
+
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    })
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
-// Escuchar petciones
+// Escuchar peticiones
 app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m','online');
 });
